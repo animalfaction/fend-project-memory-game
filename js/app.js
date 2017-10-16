fa26c8a12665a58c72398588880c06b903d7e7b4
Stephen Rutherford <s.r.rutherford@gmail.com>
2017-10-15 21:27:48 -0600

    feature: Add event listener to restart button
    Clicking the reset button now shuffles the cards.

--- js/app.js
+++ js/app.js
@@ -43,3 +43,9 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+// Event listener for restart button
+jQuery('.restart').on("click", function(event) {
+	displayCards();
+});
